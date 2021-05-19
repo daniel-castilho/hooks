f3fa23b149e8902afbb5f2cf74e3290a91797658
Daniel Castilho <dan.castilho@gmail.com>
2021-05-18 21:45:38 -0400

    Desafio Reducer

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,5 +1,6 @@
 import React, { useReducer } from "react";
 import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
 const initialState = {
 	number: 0, // foco...
@@ -14,6 +15,14 @@ function reducer(state, action) {
 			return { ...state, number: state.number + 2 };
 		case "login":
 			return { ...state, user: { name: action.payload } };
+        case "numberMult7":
+            return { ...state, number: state.number * 7 };
+        case "numberDiv25":
+            return { ...state, number: state.number / 25 };
+        case "round":
+            return { ...state, number: parseInt(state.number) };
+        case "numberAddN":
+            return { ...state, number: state.number + action.payload };
 		default:
 			return state;
 	}
@@ -28,6 +37,7 @@ const UseReducer = (props) => {
 				title="Hook UseReducer"
 				subtitle="Uma outra forma de ter estado em componentes funcionais!" />
 
+            <SectionTitle title="Exercício #01" />
 			<div className="center">
 				{state.user ? (
 					<span className="text">{state.user.name}</span>
@@ -51,6 +61,17 @@ const UseReducer = (props) => {
 					</button>
 				</div>
 			</div>
+
+            <SectionTitle title="Exercício #02" />
+            <div className="center">
+                <span className="text">{state.number}</span>
+                <div>
+                    <button className="btn" onClick={() => dispatch({ type: 'numberMult7' })}>* 7</button>
+                    <button className="btn" onClick={() => dispatch({ type: 'numberDiv25' })}>/ 25</button>
+                    <button className="btn" onClick={() => dispatch({ type: 'round' })}>Round</button>
+                    <button className="btn" onClick={() => dispatch({ type: 'numberAddN', payload: -9 })}>- 9</button>
+                </div>
+            </div>
 		</div>
 	);
 };
